bd49b462efafd484819739776d454dc1847bbb92
Alexandr Moroz <afmoroz@gmail.com>
2020-04-12 00:56:26 +0300

    interator done

--- src/pages/iterator/index.md
+++ src/pages/iterator/index.md
@@ -1,3 +1,13 @@
 ---
 title: Iterator
 ---
+
+Iterator design pattern can be viewed in two ways:
+
+1. Iterator itself as a mechanism of traversing through the data structure (ex: Symbol.Iterator for Array in JS).
+2. A way to separate the iterator mechanism from the consumer class. The class doesn't care what it has to iterate over.
+
+In JS, iterators are build-in for most data structures like arrays, sets... (not objects), so here is the example of iterator design pattern as a way to keep the iterator separate from the class that calls the iterator.  
+For more information about iterator mechanism, see resources below:  
+[ RefactoringGuru ](https://refactoring.guru/design-patterns/iterator)  
+[ DoFactory ](https://www.dofactory.com/javascript/iterator-design-pattern)
--- src/pages/iterator/index.ts
+++ src/pages/iterator/index.ts
@@ -1,109 +1,82 @@
-const arr = [1,2]
-const iteratorArr = arr[Symbol.iterator]()
-console.log(iteratorArr.next())
-console.log(iteratorArr.next())
-console.log(iteratorArr.next())
-const obj = {a: 'a', b: 'b', c: 'c'}
-const iteratorObj = Object.values(obj)[Symbol.iterator]()
-console.log(iteratorObj.next())
-console.log(iteratorObj.next())
-console.log(iteratorObj.next())
-console.log(iteratorObj.next())
-
-interface Song {
-  title: string
-  artist: string
-  year: number
-}
-
-class SongCreator {
-  constructor(public title: string, public artist: string, public year: number) {
-    return {
-      title: this.title,
-      artist: this.artist,
-      year: this.year
-    }
-  }
+class SongInfo {
+  constructor(public title: string, public artist: string, public year: number) {}
 }
 
 interface SongIterator {
-  createIterator(): void
+  createIterator(): IterableIterator<SongInfo>;
 }
 
-abstract class Songs<T> {
-  abstract bestSongs: T
-  abstract addSong(song: Song): void
-  getBestSongs(): T {
-    return this.bestSongs
-  }
-}
-
-class SongsOf70s extends Songs<Song[]> implements SongIterator{
-  bestSongs: Song[] = []
+class SongsOf70s implements SongIterator {
+  // bestSongs here is an Array;
+  bestSongs: SongInfo[] = [];
 
   constructor() {
-    super()
-    this.addSong(new SongCreator('Imagine', 'John Lennon', 1970))
-    this.addSong(new SongCreator("American Pie", "Don McLean", 1971))
-    this.addSong(new SongCreator("I Will Survive", "Gloria Gaynor", 1979))
+    this.addSong(new SongInfo("Imagine", "John Lennon", 1970));
+    this.addSong(new SongInfo("American Pie", "Don McLean", 1971));
+    this.addSong(new SongInfo("I Will Survive", "Gloria Gaynor", 1979));
   }
 
-  addSong(song: Song): void {
-    this.bestSongs.push(song)
+  addSong(song: SongInfo): void {
+    this.bestSongs.push(song);
   }
 
-  createIterator(): any {
-    return this.bestSongs[Symbol.iterator]()
+  createIterator(): IterableIterator<SongInfo> {
+    return this.bestSongs[Symbol.iterator]();
   }
 }
 
-class SongsOf80s extends Songs<{[key: string]: Song}> implements SongIterator{
-  bestSongs: {[key: string]: Song} = {}
+class SongsOf80s implements SongIterator {
+  // bestSongs here is an Object
+  bestSongs: { [key: string]: SongInfo } = {};
 
   constructor() {
-    super()
-    this.addSong(new SongCreator("Head Over Heels", "Tears For Fears", 1985))
-    this.addSong(new SongCreator("Cruel Summer", "Bananarama", 1984))
-    this.addSong(new SongCreator("Roam", "B 52s", 1989))
+    this.addSong(new SongInfo("Head Over Heels", "Tears For Fears", 1985));
+    this.addSong(new SongInfo("Cruel Summer", "Bananarama", 1984));
+    this.addSong(new SongInfo("Roam", "B 52s", 1989));
   }
 
-  addSong(song: Song): void {
-    this.bestSongs[Math.random()] = song
+  addSong(song: SongInfo): void {
+    this.bestSongs[Math.random()] = song;
   }
 
-  createIterator(): any {
-    return Object.values(this.bestSongs)[Symbol.iterator]()
+  createIterator(): IterableIterator<SongInfo> {
+    return Object.values(this.bestSongs)[Symbol.iterator]();
   }
 }
 
+class DJ {
+  constructor(public songs: SongIterator) {}
 
-class DJ<T extends SongIterator> {
-  constructor(public songs: T ) { }
-  
   printSongs() {
-    const iterator = this.songs.createIterator()
-    this.iterateSongs(iterator)
+    const iterator = this.songs.createIterator();
+    this.iterateSongs(iterator);
   }
 
   iterateSongs(iterator: any) {
-    let _value: any
-    let _done: any
-    let {value, done} = iterator.next()
-    _value = value
-    _done = done
+    let _value: any;
+    let _done: any;
+    let { value, done } = iterator.next();
+    _value = value;
+    _done = done;
 
-    while(!_done) {
-      console.log(_value?.title, _done)
-      let {value, done} = iterator.next()
-      _done = done
-      _value = value
+    while (!_done) {
+      console.log(_value);
+      let { value, done } = iterator.next();
+      _done = done;
+      _value = value;
     }
   }
 }
 
-const seventies = new SongsOf70s()
-const eighties = new SongsOf80s()
-const dj70 = new DJ(seventies)
-const dj80 = new DJ(eighties)
-dj70.printSongs()
-dj80.printSongs()
+const seventies = new SongsOf70s();
+const eighties = new SongsOf80s();
+const dj70 = new DJ(seventies);
+const dj80 = new DJ(eighties);
+
+console.group("70s");
+dj70.printSongs();
+console.groupEnd();
+
+console.group("80s");
+dj80.printSongs();
+console.groupEnd();
